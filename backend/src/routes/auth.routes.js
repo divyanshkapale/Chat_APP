ed3e9541d4ba2b51e6fc2af1b682841baeccb4a7
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-14 15:01:07 +0530

    rate limiting

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,7 +4,10 @@ import { login } from "../controller/auth.controller.js";
 import { logout } from "../controller/auth.controller.js";
 import { updateProfile } from "../controller/auth.controller.js";
 import { protectedRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = express.Router();
+
+router.use(arcjetProtection)
 
 router.post("/signup", signup);
 router.post("/login", login);
