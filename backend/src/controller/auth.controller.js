6090c787b85f90c8148cd12e332525aa782f7e62
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-14 10:50:20 +0530

    Login Logout

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../model/User.model.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import { generateToken } from "../lib/utils.js";
 import { sendWelcomeEmail } from "../email/emailHandler.js";
 
@@ -21,44 +21,78 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "Invalid email format" });
     }
 
-    const user=await User.findOne({email:email})
+    const user = await User.findOne({ email: email });
 
-    if(user){
-        return res.status(400).json({message:"Email Already exists"})
+    if (user) {
+      return res.status(400).json({ message: "Email Already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
 
-    const hashedPassword= await bcrypt.hash(password,salt)
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const newUser =new User({
-        fullName,
-        email,
-        password:hashedPassword
-    })
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
 
-    if(newUser){
-        generateToken(newUser._id,res);
-        const SavedUser=await newUser.save();
-        res.status(201).json({
-            _id:newUser._id,
-            fullName:newUser.fullName,
-            email:newUser.email,
-            profilePic:newUser.profilePic
-        })
-        try{
-            await sendWelcomeEmail(SavedUser.email,SavedUser.fullName,process.env.CLIENT_URL);
-        }
-        catch(error){
-            console.error("Failed to Send welcome Error",error);
-        }
+    if (newUser) {
+      generateToken(newUser._id, res);
+      const SavedUser = await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+      try {
+        await sendWelcomeEmail(
+          SavedUser.email,
+          SavedUser.fullName,
+          process.env.CLIENT_URL
+        );
+      } catch (error) {
+        console.error("Failed to Send welcome Error", error);
+      }
+    } else {
+      res.status(400).json({ message: "invalid User Data " });
     }
-    else {
-        res.status(400).json({message:"invalid User Data "})
-    }
-
   } catch (error) {
-    console.log("Error in Signup Controller",error)
-    res.status(500).json({message:"Internal Server Error"})
+    console.log("Error in Signup Controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const IsPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!IsPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    generateToken(user._id, res);
+    return res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+
+export const logout = async (req, res) => {
+  res.cookie("jwt","",{maxAge:0});
+  res.status(200).json({message:"Logout Successfully"})
 };
