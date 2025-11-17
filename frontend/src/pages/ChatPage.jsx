f255db176fdffc58cb3feed37a1fa4dede3f1fd3
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-17 13:48:26 +0530

    login feature added

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
-const ChatPage = () => {
+const ChatPage = () => { 
+  const { logout } = useAuthStore();
   return (
-    <div>
-      <h1>Chat Page</h1>
+    <div className="relative z-[100]">
+      <button onClick={logout}>Logout</button>
     </div>
   )
 }
+
 
 export default ChatPage
