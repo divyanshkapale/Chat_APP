dc2f2fb6f798b5350a4ea9742aba3f6ac51aaa90
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-17 23:09:37 +0530

    chat setup

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { create } from "zustand";
+
+export const useChatStore = create((set) => ({
+  allContacts: [],
+  chats: [],
+  messages: [],
+  activeTab: "chats",
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+  isSoundEnabled: JSON.parse(localStorage.getItem("isSoundEnabled")) === true,
+  toggleSound: () => {
+    localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
+    set({ isSoundEnabled: !get().isSoundEnabled });
+  },
+  setActiveTab: (tab) => set({ activeTab: tab }),
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+  getAllContacts: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/contacts");
+      set({ allContacts: res.data });
+    } catch (error) {
+      console.log("Error in fetching contacts:", error);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+  getAllChats: async () => {},
+}));
