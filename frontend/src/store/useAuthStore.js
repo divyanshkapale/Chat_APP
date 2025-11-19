77b55ec548b975c62632dfa920ad9578af4e90a8
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-19 22:17:52 +0530

    login Logout update

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -37,4 +37,30 @@ export const useAuthStore = create((set, get) => ({
       set({ isSigningUp: false });
     }
   },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+
+      toast.success("Logged in successfully");
+
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+      get().disconnectSocket();
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
+    }
+  },
 }));
