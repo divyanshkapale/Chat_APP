4f07d35ff8c02e63894e87c170a44386f4f23234
Divyanshu Kapale <divyanshkapale@gmail.com>
2025-11-19 13:05:09 +0530

    Chat setup Completed

--- src/components/MessagesLoadingSkeleton.jsx
+++ src/components/MessagesLoadingSkeleton.jsx
@@ -6,6 +6,7 @@ function MessagesLoadingSkeleton() {
           key={index}
           className={`chat ${index % 2 === 0 ? "chat-start" : "chat-end"} animate-pulse`}
         >
+            
           <div className={`chat-bubble bg-slate-800 text-white w-32`}></div>
         </div>
       ))}
