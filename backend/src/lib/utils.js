8a7b9f58525904f2f18f8b064b3d3fe6bc90eb0d
Divyanshu Kapale <divyanshkapale@gmail.com>
2026-01-29 21:28:58 +0530

    Fix Auth: Secure cookies in production

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,10 +8,10 @@ export const generateToken = (userId, res) => {
   );
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: false,           // important for localhost
-    sameSite: "lax",         // important for localhost
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in MS
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
   });
 
   return token;
